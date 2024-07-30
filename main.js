2c95c10312863e235cba8267923134bff1369a68
leo rizzo <leo.g.rizzorivas@gmail.com>
2024-07-30 17:34:31 -0300

    estructura de datos practica

---
+++ main.js
@@ -0,0 +1,8 @@
+/*let array = ['nombre','apellido','edad']
+console.log (array)
+alert(array)*/
+let nombre = prompt('ingrese su nombre')
+let apellido = prompt('ingrese su apellido')
+let edad = parseInt(prompt('ingrese su edad'))
+let array2 = [nombre,apellido,edad]
+alert('hola'+' '+ array2)
